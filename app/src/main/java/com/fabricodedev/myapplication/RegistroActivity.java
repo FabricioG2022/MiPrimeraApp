438155e884264bfa08457340be9334c2e251cedc
FabricioG2022 <firgodoy@hotmail.com>
2025-10-18 23:12:18 -0300

    feat: se agrego registro java y registro xml

---
+++ app/src/main/java/com/fabricodedev/myapplication/RegistroActivity.java
@@ -0,0 +1,32 @@
+package com.fabricodedev.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    private TextView inicioLink;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        inicioLink = findViewById(R.id.tv_inicia_sesion_aqui);
+        inicioLink.setOnClickListener(v -> {
+            Intent intent = new Intent(RegistroActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+    }
+}
