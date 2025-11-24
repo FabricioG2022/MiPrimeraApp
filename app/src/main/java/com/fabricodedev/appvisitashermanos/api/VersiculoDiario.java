426c80eecdfe8d6f7916195c199a2e6681d88d8b
FabricioG2022 <firgodoy@hotmail.com>
2025-11-24 16:39:34 -0300

    feat: Se cambio el manejo de los text por el uso de string en todos los layouts

--- app/src/main/java/com/fabricodedev/appvisitashermanos/api/VersiculoDiario.java
+++ app/src/main/java/com/fabricodedev/appvisitashermanos/api/VersiculoDiario.java
@@ -3,29 +3,10 @@ package com.fabricodedev.appvisitashermanos.api;
 import com.google.gson.annotations.SerializedName;
 
 public class VersiculoDiario {
-
     @SerializedName("text")
-    private String texto; // El texto del versículo
+    private String texto;
 
-    @SerializedName("reference")
-    private String referencia; // La referencia, ej: "Juan 3:16"
-
-    @SerializedName("translation_name")
-    private String traduccion; // Nombre de la versión (opcional)
-
-    // Constructor vacío
-    public VersiculoDiario() {}
-
-    // Getters
     public String getTexto() {
         return texto;
-    }
-
-    public String getReferencia() {
-        return referencia;
-    }
-
-    public String getTraduccion() {
-        return traduccion;
     }
 }
